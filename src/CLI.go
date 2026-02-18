f5849dd775808e33580dbabc93ac33dffeb8d236
CartridgeSmoker <montoguyps4@gmail.com>
2026-02-18 11:52:57 +0500

    add CLI.go

---
+++ src/CLI.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+type Command interface {
+	Name() string
+	Description() string
+	Execute(args []string) error
+}
+
+type CLI struct {
+	commands map[string]Command
+}
+
+func MakeCommandRegistry(cmds ...Command) *CLI {
+	registry := make(map[string]Command)
+	for _, cmd := range cmds {
+		registry[cmd.Name()] = cmd
+	}
+	return &CLI{commands: registry}
+}
+
+func (cli *CLI) Execute() {
+	if len(os.Args) < 2 {
+		log.Println("Usage: cloaq <command>")
+		return
+	}
+
+	commandName := os.Args[1]
+	command, exists := cli.commands[commandName]
+	if !exists {
+		log.Println("Unknown command:", commandName)
+	}
+
+	if err := command.Execute(os.Args[2:]); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type RunCommand struct{}
+
+func (r *RunCommand) Name() string {
+	return "run"
+}
+
+func (r *RunCommand) Description() string {
+	return "Start Cloaq gateway"
+}
+
+func (r *RunCommand) Execute(args []string) error {
+	if os.Geteuid() != 0 {
+		return fmt.Errorf("run as root")
+	}
+
+	log.Println("Running Cloaq")
+
+	tunFD := NewTUN("tun0")
+
+	AddRoute("2001:db8:1::/64", "eth0")
+	AddRoute("2001:db8:2::/64", "eth1")
+
+	log.Println("IPv6 TUN gateway created")
+
+	CreateIPv6PacketListener(tunFD)
+
+	return nil
+}
+
+type HelpCommand struct {
+	cli *CLI
+}
+
+func (h *HelpCommand) Name() string {
+	return "help"
+}
+
+func (h *HelpCommand) Description() string {
+	return "Show help information"
+}
+
+func (h *HelpCommand) Execute(args []string) error {
+	for _, cmd := range h.cli.commands {
+		log.Printf("%s - %s\n", cmd.Name(), cmd.Description())
+	}
+	return nil
+}
+
+type SettingsCommand struct{}
+
+func (s *SettingsCommand) Name() string {
+	return "settings"
+}
+
+func (s *SettingsCommand) Description() string {
+	return "Display configuration settings"
+}
+
+func (s *SettingsCommand) Execute(args []string) error {
+	log.Println("----- [SETTINGS] -----")
+	return nil
+}
