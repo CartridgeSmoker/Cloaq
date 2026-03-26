10184bc5ba9cdd20813a5692496ef552bc2c43a1
Danil Kolbasenko <nasarspektrreas@gmail.com>
2026-03-26 22:53:16 +0500

    feat:Updated packet.go ciphering key

--- src/utils/packet.go
+++ src/utils/packet.go
@@ -1,8 +1,12 @@
 package utils
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
 	"encoding/binary"
 	"fmt"
+	"io"
 )
 
 type Packet struct {
@@ -10,24 +14,40 @@ type Packet struct {
 	Version uint8
 }
 
-// Encapsulate adds a 4-byte header: [version][type][len_high][len_low]
-func Encapsulate(data []byte) ([]byte, error) {
+func Encapsulate(data []byte, key []byte) ([]byte, error) {
 	size := len(data)
-	var version byte
-	var msgType byte
+	const version byte = 1
+	const msgType byte = 2
 
 	if size > 65535 {
 		return nil, fmt.Errorf("payload too large: %d bytes", size)
 	}
 
-	buf := make([]byte, 4+size)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
 
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	encryptedData := gcm.Seal(nil, nonce, data, nil)
+	finalSize := len(encryptedData) + len(nonce)
+
+	buf := make([]byte, 4+finalSize)
 	buf[0] = version
 	buf[1] = msgType
+	binary.BigEndian.PutUint16(buf[2:4], uint16(finalSize))
 
-	binary.BigEndian.PutUint16(buf[2:4], uint16(size))
-
-	copy(buf[4:], data)
+	copy(buf[4:4+len(nonce)], nonce)
+	copy(buf[4+len(nonce):], encryptedData)
 
 	return buf, nil
 }
